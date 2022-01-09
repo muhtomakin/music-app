86858c679369122e07df3f4c3f847f661c118cc0
muhtomakin <muhtomakin@gmail.com>
2022-01-09 13:12:09 +0100

    version 1

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.scss';
+import Nav from './components/Nav';
+import Main from './components/Main';
+
+function App() {
+  return (
+    <div className="outerWrap">
+      <div className="App">
+        <Nav />
+        <Main />
+      </div>
+      <div className="musicControls">music controls</div>
+    </div>
+  );
+}
+
+export default App;
